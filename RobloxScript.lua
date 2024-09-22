3a4eefa472251921118526e2cbb529036d1d334b
Noah <108598167+TheFakeFew@users.noreply.github.com>
2024-09-22 05:51:07 -0400

    Update RobloxScript.lua

--- RobloxScript.lua
+++ RobloxScript.lua
@@ -151,7 +151,7 @@ if(not getfenv().owner)then
 end
 
 function getInstruments()
-    local data = game:GetService('HttpService'):GetAsync("https://raw.githubusercontent.com/OrangeCash090/MIDI-Player-Roblox/main/Instruments.lua")
+    local data = game:GetService('HttpService'):GetAsync("https://raw.githubusercontent.com/TheFakeFew/ClientMidi/main/Core/Families.lua")
     local DATA = loadstring(data or "")()
     return DATA or nil
 end
